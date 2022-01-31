1895953794a7ad45d61321d4e4f49890bb159db4
Mr.Steelcan <mr.steelcan@gmail.com>
2022-01-31 16:03:36 +0200

    Update headerSlice.js

--- store/headerSlice.js
+++ store/headerSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const HeaderSlice = createSlice({
   name: 'header',
   initialState: {
-    value: '/',
+    value: '',
   },
   reducers: {
     changeValue: (state, action) => {

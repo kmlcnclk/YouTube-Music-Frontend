33934ebb7b2086008af83abfbda17eab39e94b0e
Mr.Steelcan <mr.steelcan@gmail.com>
2022-02-01 19:36:03 +0200

    Update HomeComponent.jsx

--- components/Home/HomeComponent.jsx
+++ components/Home/HomeComponent.jsx
@@ -91,9 +91,7 @@ class HomeComponent extends Component {
   };
 
   getMusicFunc = async () => {
-    const res = await unfetch(
-      `https://youtube-music-backend.herokuapp.com/music/get20Music`
-    );
+    const res = await unfetch(`${process.env.BACKEND_URL}/music/get20Music`);
     const data = await res.json();
 
     if (data) {

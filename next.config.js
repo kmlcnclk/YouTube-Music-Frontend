8242ff8b1364d547620baa4b3ae286a9485edfc3
Kamilcan Çelik <56474929+KamilcanCelik@users.noreply.github.com>
2022-02-02 11:10:40 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-module.exports = {
-  reactStrictMode: false,
-  images: {
-    domains: ['youtube-music-backend.herokuapp.com'],
-  },
+const { SERVER_DIRECTORY } = require('next/constants');
+
+module.exports = (phase, { defaultConfig }) => {
+  return {
+    reactStrictMode: false,
+    images: {
+      domains: ['youtube-music-backend.herokuapp.com'],
+    },
+  };
 };
